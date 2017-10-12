d96111da6570db97c9abf7f24a70629bff0a2a67
Phung Minh Tran phung.tran@amblique.com <phung.tran@amblique.com>
2017-10-12 17:33:47 +0700

    loop

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import Cource from './components/Cource'
 
 class App extends Component {
-  render() {
-    return (
-        <div className="row">
-            <Cource />
-            <Cource />
-            <Cource />
-        </div>
-    );
-  }
+    render() {
+        var cources = [
+            {
+                name: "React JS",
+                free: true
+            },
+            {
+                name: "Angular JS",
+                free: false
+            },
+            {
+                name: "Node JS",
+                free: true
+            }
+        ];
+
+        var courceEles = cources.map((cource, index) =>
+            <Cource key={index} name={cource.name} free={cource.free} />
+        )
+
+        return (
+            <div className="row">{courceEles}</div>
+        );
+    }
 }
 export default App;
--- src/components/Cource.js
+++ src/components/Cource.js
@@ -2,24 +2,31 @@ import React, { Component } from 'react';
 import Lession from './Lession'
 
 class Cource extends Component {
-  render() {
-    return (
-        <div className="col-xs-4 col-md-4 col-lg-4">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">React JS</h3>
-              </div>
-              <div className="panel-body">
-                <ul className="list-group">
-                  <Lession />
-                  <Lession />
-                  <Lession />
-                </ul>
-              </div>
+    showRegistryBtn() {
+        const isFree = this.props.free;
+        if(isFree) {
+            return <button className="btn btn-default">Registry</button>
+        }
+    }
+
+    render() {
+        return (
+            <div className="col-xs-4 col-md-4 col-lg-4">
+                <div className="panel panel-default">
+                  <div className="panel-heading">
+                    <h3 className="panel-title">{this.props.name}</h3>
+                  </div>
+                  <div className="panel-body">
+                    <ul className="list-group">
+                      <Lession />
+                      <Lession />
+                      <Lession />
+                    </ul>
+                  </div>
+                </div>
+                {this.showRegistryBtn()}
             </div>
-            <button className="btn btn-default">Registry</button>
-        </div>
-    );
-  }
+        );
+    }
 }
 export default Cource;
