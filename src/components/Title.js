1a5a9170f8ba5cd5b76d32806272f38fb91b6bc5
Phung Minh Tran phung.tran@amblique.com <phung.tran@amblique.com>
2018-01-02 13:56:45 +0700

    - Phân chia Component của ứng dụng.

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+    render() {
+
+        return (
+            <div className='row'>
+                <div className='page-header'>
+                   <h1>Project 01 - ToDo List <small>ReactJS</small></h1>
+                </div>
+            </div>
+        );
+    }
+}
+export default Title;
