f00516118c1fe927b9157a560fe3748c3b704c23
DESKTOP-HLKGEMR\Admin <phungthaouit@gmail.com>
2018-03-08 23:08:52 +0700

    Refactor code.

--- src/reducers/order.js
+++ src/reducers/order.js
@@ -6,9 +6,10 @@ var defaultState = {
 export let order = (state = defaultState, action) => {
 	switch(action.type) {
 		case 'ORDER':
-			defaultState.orderBy = action.orderBy;
-			defaultState.orderDir = action.orderDir;
-			return state;
+			return {
+				orderBy: action.orderBy,
+				orderDir: action.orderDir
+			};
 			
 		default:
 			return state;
