34b431418380f97ec9d0670f731327c426e9dcb5
DESKTOP-HLKGEMR\Admin <phungthaouit@gmail.com>
2018-03-08 23:25:59 +0700

    - Redux 05

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,4 @@
+export const CLOSE_FORM = 'CLOSE_FORM';
+export const OPEN_FORM = 'OPEN_FORM';
+export const TOGGER_FORM = 'TOGGER_FORM';
+export const ORDER = 'ORDER';
--- src/reducers/isShowForm.js
+++ src/reducers/isShowForm.js
@@ -1,16 +1,18 @@
+import * as types from './../constants/ActionTypes';
+
 var defaultState = false;
 
 export let isShowForm = (state = defaultState, action) => {
 	switch(action.type) {
-		case 'CLOSE_FORM':
+		case types.CLOSE_FORM:
 			state = false;
 			return state;
 
-		case 'OPEN_FORM':
+		case types.OPEN_FORM:
 			state = true;
 			return state;
 
-		case 'TOGGER_FORM':
+		case types.TOGGER_FORM:
 			state = !state;
 			return state;
 			
