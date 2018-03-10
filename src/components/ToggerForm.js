5c906c1a5d5675970d672d881cc29c171dfb3d99
DESKTOP-HLKGEMR\Admin <phungthaouit@gmail.com>
2018-03-10 23:13:47 +0700

    - Open - Close

--- src/components/ToggerForm.js
+++ src/components/ToggerForm.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { AcToggerForm } from '../actions/index';
 
 class ToggerForm extends Component {
 
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state 				= {};
+		this.handleToggerForm	= this.handleToggerForm.bind(this); 
+	}
+
+	handleToggerForm() {
+		this.props.toggerForm();
 	}
 
 	render() {
@@ -15,7 +21,7 @@ class ToggerForm extends Component {
 
 		return (
 			<div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-				<button onClick={this.handleAdd} type="button" className={`btn ${btnClass} btn-block`}>{btnName}</button>
+				<button onClick={this.handleToggerForm} type="button" className={`btn ${btnClass} btn-block`}>{btnName}</button>
 			</div>
 		)
 	}
@@ -29,4 +35,12 @@ var mapStateToProps = function(state) {
 	}
 }
 
-export default connect(mapStateToProps)(ToggerForm);
+var mapDispatchToProps = dispatch => {
+	return {
+		toggerForm: () => {
+			dispatch(AcToggerForm())
+		}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ToggerForm);
--- src/reducers/isShowForm.js
+++ src/reducers/isShowForm.js
@@ -1,18 +1,18 @@
-import * as types from './../constants/ActionTypes';
+import * as Types from '../constants/ActionTypes';
 
 var defaultState = false;
 
 export let isShowForm = (state = defaultState, action) => {
 	switch(action.type) {
-		case types.CLOSE_FORM:
+		case Types.CLOSE_FORM:
 			state = false;
 			return state;
 
-		case types.OPEN_FORM:
+		case Types.OPEN_FORM:
 			state = true;
 			return state;
 
-		case types.TOGGER_FORM:
+		case Types.TOGGER_FORM:
 			state = !state;
 			return state;
 			
